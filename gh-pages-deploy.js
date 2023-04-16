f14121a930ac42ff331dff5e887af5153d429bfc
naroop <nrupp815@outlook.com>
2023-04-16 00:52:54 -0500

    typo

--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -14,7 +14,7 @@ const fs = require("fs");
 // List of files, regex find & replaces to perform for Github pages
 const replaces = [
   {
-    file: "./src/router.js",
+    file: "./src/router/router.js",
     find: "mode: 'history'",
     replace: "mode: 'hash'",
   },
